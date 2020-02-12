b7b975cee29cc337557bea9257c62b6475c24ba5
arizaga <adryel.arizaga@jpl.nasa.gov>
2020-02-12 10:39:56 -0800

    Added script to assess best matches to unspsc slim xls file

---
+++ scripts/assign_most_similar.py
@@ -0,0 +1,55 @@
+import xlwt
+from itertools import chain
+
+from load_excel import UNSPSC_SLIM, COMM
+from compare_excels import compare_from_row
+
+FILE_NAME = "UNSPSC_randSample.xls"
+EXTENSION_LOC = '../sample_text/'
+BASE_COL = 'Class Title'
+E_COMM_COL = 'COMM_DSCR_UP'
+
+
+def assign_best_to_rows():
+    all_rows = []
+    UNSPSC_SLIM.set_workbook()
+    fields = UNSPSC_SLIM.fields
+    additional_fields = [[f'Best_Match #{i}', f'Percent_Similarity #{i}', f'eComm_CD #{i}'] for i in range(1, 6)]
+    additional_fields = list(chain.from_iterable(additional_fields))
+
+    count = 0
+    for row in UNSPSC_SLIM.get_all_rows():
+        if count % 100 == 0:
+            print(f'Finished {count} evaluations')
+        count += 1
+
+        cur_row_val = [row[f] for f in fields]
+        top_sims = compare_from_row(row, COMM, BASE_COL, E_COMM_COL, save_num=5)
+        add_vals = [[title, round(sim, 3), ecomm['COMM_CD']] for (sim, title, ecomm) in top_sims[::-1]]
+        cur_row_val += list(chain.from_iterable(add_vals))
+
+        all_rows.append(cur_row_val)
+
+    fields += additional_fields
+
+    return all_rows, fields
+
+
+def write_best_to_file():
+    all_rows, fields = assign_best_to_rows()
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet('Main')
+    for index, value in enumerate(fields):
+        sheet.write(0, index, value)
+
+    print('Writing to file...')
+
+    for i, row in enumerate(all_rows, start=1):
+        for j, cell in enumerate(row):
+            sheet.write(i, j, cell)
+
+    workbook.save(EXTENSION_LOC + 'UNSPSC_most_similar.xls')
+
+
+if __name__ == '__main__':
+    write_best_to_file()
--- scripts/compare_excels.py
+++ scripts/compare_excels.py
@@ -1,6 +1,15 @@
-from load_excel import COMM, UNSPSC_SLIM, UNSPSC, ExcelFile
+from load_excel import COMM, UNSPSC_SLIM, ExcelFile
 import time
 import compare_sentences as cs
+
+from typing import Dict, Any
+
+
+def sort(sims):
+    if not sims:
+        return []
+    pivot = sims[0]
+    return [l for l in sims[1:] if l[0] < pivot[0]] + [pivot] + [m for m in sims[1:] if m[0] >= pivot[0]]
 
 
 def compare_excels(excel1: ExcelFile, excel2: ExcelFile, col_name1: str, col_name2: str, save_num=10):
@@ -8,24 +17,36 @@ def compare_excels(excel1: ExcelFile, excel2: ExcelFile, col_name1: str, col_nam
     Get the defined column from a random row from the first Excel File (the base comparator).
         Then iterate through the rows in the second excel files and compare the base comparator
         to the designated column
-    :return: None
+    The functions of 'compare_excel' has been abstracted into 3 layers:
+        'compare_excels': Gets most similar rows in excel2 for a RANDOM row in excel1
+        'compare_from_row': Returns most similar rows in excel2 for a given row
+        'compare_from_cell': Returns most similar rows in excel2 for a given string
+    :return: (Dict[str, Any], List[Tuple[float, str, Dict]])
     """
 
     base_row = excel1.get_row()
-    base = base_row[col_name1]
+    top_sim = compare_from_row(base_row, excel2, col_name1, col_name2, save_num=save_num)
+
+    return base_row, top_sim
+
+
+def compare_from_row(base_row: Dict[str, Any], other_excel: ExcelFile, base_col: str, other_col: str, save_num=10):
+    base = base_row[base_col]
+    return compare_from_cell(base, other_excel, other_col, save_num=save_num)
+
+
+def compare_from_cell(base: str, other_excel: ExcelFile, other_col: str, save_num=10):
     top_sim = []
 
-    for row in excel2.get_all_rows():
-        similarity = cs.deep_compare(base, row[col_name2])
+    for row in other_excel.get_all_rows():
+        similarity = cs.deep_compare(base, row[other_col])
         if len(top_sim) <= save_num or similarity > top_sim[0][0]:
-            top_sim.append((similarity, row[col_name2], row))
-            top_sim.sort()
+            top_sim.append((similarity, row[other_col], row))
+            top_sim = sort(top_sim)
             if len(top_sim) > save_num:
                 del top_sim[0]
-    #
-    # print(base_row)
-    # print(base)
-    return base_row, top_sim
+
+    return top_sim
 
 
 if __name__ == "__main__":
@@ -38,7 +59,4 @@ if __name__ == "__main__":
     print(base_row)
     print("Class Title:", base_row[U_COLUMN_NAME])
     for sim, col, row in top_sim[::-1]:
-        print(f'Similarity: {sim}, DESC: {col}')
-        for k in ['COMM_CD', 'COMM_CLS', 'COMM_ITM']:
-            print(f'{k}: {row[k]}', end=", ")
-        print()
+        print(f'Similarity: {sim}, COMM_CD: {row["COMM_CD"]}, DESC: {col}')
