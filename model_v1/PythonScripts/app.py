8d96714769e3643bac7ad31651641deadf969c8a
JesseRodriguezAvila <jesserodriguezavila@gmail.com>
2020-02-17 08:47:56 -0800

    model version 3

---
+++ model_v1/PythonScripts/app.py
@@ -0,0 +1,10 @@
+import wordNet
+
+list1 = [['school', 'school'], ['supply', 'supply'], ['set', 'school'], ['kit', 'equipment']]
+
+#list2 = wordNet.results(list1)
+
+list3 = wordNet.average_percentage(list1)
+
+#print(list2)
+print(list3)
