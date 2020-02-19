a000db1d37bca9b29670da5b4af33569dd5018b5
DanielGMagallanes <dgmdragon@gmail.com>
2020-02-19 02:10:24 -0800

    fix issue

--- textdoctest.py
+++ textdoctest.py
@@ -1,20 +1,49 @@
-def computeGCD(x, y):
-    gcd=0
-    if x > y:
-        small = y
-    else:
-        small = x
-    for i in range(1, small+1):
-        if((x % i == 0) and (y % i == 0)):
-            gcd = i
-    return gcd
+import os
+from gensim.models import LsiModel
+from gensim import similarities,models
+import gensim
+from gensim.corpora import MmCorpus
+import warnings
+import time
+import nltk
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from gensim import corpora
+import operator
+warnings.simplefilter('ignore')
+
+import MakerOfFiles as mf
+
+test = "MASS TRANSPORTATION - RAIL VEHICLE PARTS AND ACCESSORIES"
+test = test.lower()
+query_doc = word_tokenize(test)
 
 
-numbers = [iter for iter in range(31200)]
-values=[]
-for num in numbers:
-    result = computeGCD(31200,num)
-    if(result == 1):
-        values.append(num)
+listNew = []
 
-print(values)
+
+def reduceList(lengths,sortlist, dictionarys,corpuses):
+    for i in range(len(lengths)):
+        mm = MmCorpus(corpuses[i])
+        tf_idf = models.TfidfModel(mm)
+        load_dic = corpora.Dictionary.load_from_text(dictionarys[i])
+        lsi = LsiModel(mm,num_topics=lengths[i],id2word = load_dic)
+        index = gensim.similarities.MatrixSimilarity(lsi[mm],num_features=lengths[i])
+        query_doc_bow = load_dic.doc2bow(query_doc, True)
+        query_doc_tf_idf = tf_idf[query_doc_bow]
+        sim = list(zip(sortlist[i],index[lsi[query_doc_tf_idf]]))
+        sim.sort(key = operator.itemgetter(1),reverse = True)
+        listNew.append(sim[0:5])
+        
+
+start2=time.time()
+reduceList(mf.lengths,mf.sortlist,mf.Dictlist,mf.Corlist)
+    
+       
+print(f'Narrowing done in {time.time() - start2}')
+
+exceltest = pd.ExcelWriter("test2.xlsx",engine='xlsxwriter')
+data= pd.DataFrame(listNew)
+data.to_excel(exceltest,sheet_name='MASS TRANSPORTATION')
+
+exceltest.save()
